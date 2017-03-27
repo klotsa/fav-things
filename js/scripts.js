cbd246a42eaf9f7fb2e61c6637ed24f88da58d06
Anatoliy Klots <klots@uw.edu>
2017-03-27 13:35:34 -0700

    add arrays

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,21 @@ $(document).ready(function() {
     var drinkInput = $("input#drink").val();
     var filmInput = $("input#film").val();
 
+    var favorites = [];
+    var favs2 = [];
+
+
+    favorites.push(foodInput, drinkInput, filmInput);
+    favs2.push(favorites[1], favorites[2]);
+
 
     $(".food").text(foodInput);
     $(".drink").text(drinkInput);
     $(".film").text(filmInput);
-
+    $(".favorites").text(favorites);
+    $(".favs2").text(favs2);
+    $("ul").append("<li>" + favorites[0] + "</li>");
+    $("ul").append("<li>" + favorites[1] + "</li>");
 
     $("#output").show() ;
 
